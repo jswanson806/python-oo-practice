f0b748148a458bb1cdc400528ba5fcc75066e197
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-10-22 13:28:12 -0500

    finished serial exercise

--- serial.py
+++ serial.py
@@ -1,5 +1,7 @@
 """Python serial number generator."""
 
+
+
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
     
@@ -19,4 +21,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    # this variable tracks how many times we have called the generate method
+    count = 0
 
+    def __init__(self, start):
+        self.start = start
+    
+    def generate(self):
+        """returns the next incremental number from the starting number"""
+        num = self.start
+        self.start += 1
+        self.count += 1
+        return num
+
+    def reset(self):
+        """resets the generator back to the starting number"""
+        self.start = self.start - self.count
